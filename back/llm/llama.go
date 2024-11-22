144ff1894c9519d40ff1673b3d09bda9d8cde60f
Ricardo <ricardosantossiqueira@poli.ufrj.br>
2024-11-21 22:49:39 -0300

    sys prompt update

--- back/llm/llama.go
+++ back/llm/llama.go
@@ -3,23 +3,10 @@ package llm
 import (
 	"context"
 	"os"
-	"strings"
 
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/option"
 )
-
-var systemPrompt = strings.Join([]string{
-	"You are to generate a flask web service.",
-	"Requirements:",
-	"1. The service should run on port 5000.",
-	"2. It must be a single Python file.",
-	"3. The service should have only one route at the root ('/').",
-	"4. Only GET or POST requests are allowed.",
-	"5. Return ONLY the Python code, without formatting or explanations.",
-	"6. Responses MUST ALWAYS be in JSON fromat.",
-	"7. DO NOT format code using markdown ```.",
-}, " ")
 
 func Run(userMessage string) (*openai.ChatCompletion, error) {
 	apiKey := os.Getenv("LLAMA_API_KEY")
@@ -28,13 +15,15 @@ func Run(userMessage string) (*openai.ChatCompletion, error) {
 		option.WithBaseURL("https://api.llama-api.com"),
 	)
 
+	systemPrompt1, err := os.ReadFile("llm/prompts/system_prompt_1.md")
+	if err != nil {
+		return nil, err
+	}
+
 	chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
-		MaxTokens:        openai.Int(2000),
-		Temperature:      openai.Float(0.1),
-		TopP:             openai.Float(1.0),
-		FrequencyPenalty: openai.Float(1.0),
+		MaxTokens: openai.Int(2000),
 		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
-			openai.FunctionMessage("System", systemPrompt),
+			openai.SystemMessage(string(systemPrompt1)),
 			openai.UserMessage(userMessage),
 		}),
 		Model: openai.F("llama3.1-70b"),
